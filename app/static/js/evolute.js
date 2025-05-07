2f864bee77d2d6567447e91a6037e44ba06edd38
Gleicimar <gleicimarribeiro@gmail.com>
2025-05-07 18:42:15 -0300

    adicionado icone de menu e corrigido a reponsividade 3 javascript e menu mobile

---
+++ app/static/js/evolute.js
@@ -0,0 +1,8 @@
+const  Menu = document.querySelector(".menu")
+const   nav_menu = document.getElementById('menu')
+
+Menu.addEventListener("click", ()=>{
+    nav_menu.classList.toggle("active")
+    /*alert("clicado")*/
+})
+
