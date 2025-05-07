f8bec3adcc3962af3157d48e0f57c60bb3de2d1b
Gleicimar <gleicimarribeiro@gmail.com>
2025-05-06 22:08:59 -0300

    adicionado icone de menu e corrigido a reponsividade

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,18 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, flash, render_template, request, flash, get_flashed_messages
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def home():
     return render_template('home.html')
-
+@main.route('/login',methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username= request.form['usuario']
+        password = request.form['senha']
+        if username =="admin" or password =="admin":
+            flash('Usuario ou senha incorretos')
+            return render_template('login.html')
+        flash('Login efetuado com sucesso')
+        return render_template('home.html')
+    return render_template('login.html')
