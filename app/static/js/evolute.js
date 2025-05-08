52b45ed26c68c4367348f31255712b2b85b501cc
Gleicimar <gleicimarribeiro@gmail.com>
2025-05-08 18:49:28 -0300

    sorrigido espaçamento e adicionado o'x' para fechar o menu

--- app/static/js/evolute.js
+++ app/static/js/evolute.js
@@ -1,8 +1,16 @@
 const  Menu = document.querySelector(".menu")
 const   nav_menu = document.getElementById('menu')
-
+const  icon  =Menu.querySelector('i')
 Menu.addEventListener("click", ()=>{
     nav_menu.classList.toggle("active")
-    /*alert("clicado")*/
+    if (icon.classList.contains('fa-bars')){
+        icon.classList.remove('fa-bars');
+        icon.classList.add("fa-times");
+    }else{
+        icon.classList.remove("fa-times");
+        icon.classList.add("fa-bars");
+
+    }
+
 })
 
