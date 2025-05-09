fa85efbaaafb6a014c31a482a969fb0502b08227
Gleicimar <gleicimarribeiro@gmail.com>
2025-05-08 22:36:47 -0300

    atualizado o menu do topo e rodapé  e incluido a página de login

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, flash, render_template, request, flash, get_flashed_messages
+from flask import Blueprint, flash, render_template, request, flash, get_flashed_messages,redirect, url_for
 
 main = Blueprint('main', __name__)
 
@@ -7,12 +7,18 @@ def home():
     return render_template('home.html')
 @main.route('/login',methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        username= request.form['usuario']
-        password = request.form['senha']
-        if username =="admin" or password =="admin":
-            flash('Usuario ou senha incorretos')
+    if request.method =='POST':
+        usuario =request.form['username']
+        senha = request.form['password']
+        if usuario == 'admin' and senha =='admin':
+            return redirect(url_for('painel'))
+        else:
+            flash('Usuário ou senha incorretos')
             return render_template('login.html')
-        flash('Login efetuado com sucesso')
-        return render_template('home.html')
     return render_template('login.html')
+
+
+
+@main.route('/painel')
+def painel():
+    return render_template('painel.html')
--- app/static/js/evolute.js
+++ app/static/js/evolute.js
@@ -1,6 +1,7 @@
 const  Menu = document.querySelector(".menu")
 const   nav_menu = document.getElementById('menu')
-const  icon  =Menu.querySelector('i')
+const  icon  = Menu.querySelector('i')
+
 Menu.addEventListener("click", ()=>{
     nav_menu.classList.toggle("active")
     if (icon.classList.contains('fa-bars')){
