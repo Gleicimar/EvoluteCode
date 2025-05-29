845553da0acf8018c1280c626177518dac4b5d76
Gleicimar <gleicimarribeiro@gmail.com>
2025-05-29 18:30:08 -0300

    corrigido identação erro3

--- app/models/mongo.py
+++ app/models/mongo.py
@@ -2,7 +2,7 @@ import os
 from pymongo import MongoClient
 from config import Config
 
- MONGO_URI = os.getenv('MONGO_URI')
+MONGO_URI = os.getenv('MONGO_URI')
     if not MONGO_URI:
       raise ValueError("Variável MONGO_URI não está definida")
     print("Conectado com sucesso")
