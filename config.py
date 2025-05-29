80428c101a0130c3569958f1fa5333cb2ef983df
Gleicimar <gleicimarribeiro@gmail.com>
2025-05-29 18:08:58 -0300

    corrigido identação erro

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 class Config:
-   MONGO_URI = os.getenv('MONGO_URI')
+    MONGO_URI = os.getenv('MONGO_URI')
     if not MONGO_URI:
       raise ValueError("Variável MONGO_URI não está definida")
     print("Conectado com sucesso")
